9c40191c22f02d6e8dceb367809f57c987b64f0b
Cheryl Chandra <cchandra@rhythmone.com>
2019-07-19 13:53:35 -0700

    add html

--- hello.py
+++ hello.py
@@ -41,6 +41,18 @@ def hello_user(name):
   else:
     return redirect(url_for('hello_guest',guest = name))
 
+@app.route('/success/<name>')
+def success(name):
+  return 'welcome %s' % name
+
+@app.route('/login',methods = ['POST','GET'])
+def login():
+  if request.method == 'POST':
+    user = request.form['nm']
+    return redirect(url_for('success',name = user))
+  else:
+    user = request.args.get('nm')
+    return redirect(url_for('success',name = user))
 
 if __name__ == '__main__':
   app.run(debug = True)
